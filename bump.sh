81d0180d44d32498d8ef478c582e8cae28dd98d2
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-02-19 22:29:42 +0900

    fix

--- bump.sh
+++ bump.sh
@@ -26,7 +26,7 @@ tolower($0) ~ /ver.* /,/NF/ {
 
 sed -i "/compile/s/[0-9]*\.[0-9]*\.[0-9]*/$VERSION/" ./README.md
 
-openssl aes-256-cbc -K $encrypted_5ef410394863_key -iv $encrypted_5ef410394863_iv -in travis_rsa.enc -out ~\/.ssh/travis_rsa -d
+openssl aes-256-cbc -K $encrypted_5ef410394863_key -iv $encrypted_5ef410394863_iv -in travis_rsa.enc -out ~/.ssh/travis_rsa -d
 chmod 600 ~/.ssh/id_rsa
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
